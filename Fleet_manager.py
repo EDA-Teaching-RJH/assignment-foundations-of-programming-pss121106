5d325d20fffb2614d0a5702fd70bed84933b2756
pss121106 <pss1@kent.ac.uk>
2026-02-11 21:40:59 +0000

    Feature 2

--- Fleet_manager.py
+++ Fleet_manager.py
@@ -6,3 +6,17 @@ def init_database():
     divs  = ["Command", "Command", "Operations", "Security", "Engineering"]
     ids   = ["001", "002", "003", "004", "005"]
     return names, ranks, divs, ids
+
+#Feature 2
+def display_menu():
+    print("\n--- FLEET COMMAND MENU ---")
+    print("1. View Roster")
+    print("2. Add Crew Member")
+    print("3. Remove Crew Member")
+    print("4. Update Rank")
+    print("5. Search Crew")
+    print("6. Filter by Division")
+    print("7. Calculate Payroll")
+    print("8. Count High Officers")
+    print("9. Exit")
+    return input("Select option: ")
