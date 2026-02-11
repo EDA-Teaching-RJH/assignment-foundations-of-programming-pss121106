24f46eba8cbe63149eb6e82f0b8801eb64efe480
pss121106 <pss1@kent.ac.uk>
2026-02-11 21:31:19 +0000

    Feature 1

---
+++ Fleet_manager.py
@@ -0,0 +1,8 @@
+#Feature 1
+import time
+def init_database():
+    names = ["Michael Townley", "Trevor Phillips", "Franklin Clinton", "Lamar Davis", "Lester Crest"]
+    ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Lt. Commander"]
+    divs  = ["Command", "Command", "Operations", "Security", "Engineering"]
+    ids   = ["001", "002", "003", "004", "005"]
+    return names, ranks, divs, ids
